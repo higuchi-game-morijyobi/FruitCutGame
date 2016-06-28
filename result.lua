7a8dc0d653ad90f6b9b74bda618952d24c991f20
Taiki Arakawa <at.090313@gmail.com>
2016-06-28 11:37:26 +0900

    result.lua local変数追加

--- result.lua
+++ result.lua
@@ -1,1 +1,28 @@
-result.lua
+
+-----------------------------------------------------------------------------------------
+--
+-- result.lua
+--
+-----------------------------------------------------------------------------------------
+
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+--音の読み込み
+local title_bgm=media.playSound("bgm/AQUARIUM.mp3")
+local SE_SELECT = media.newEventSound("se/SE_Select.mp3")
+
+local fruits = require("fruits")
+
+--------------------------------------------
+
+local screenW, screenH, halfW, halfH = display.contentWidth, display.contentHeight, display.contentWidth*0.5, display.contentHeight*0.5
+
+local title_button= display.newImage("images/title_button.png" ,halfW, screenH*0.7)
+local resultText=display.newText("りざると！",halfW, screenH*0.1,nil,48)
+      resultText:setTextColor(255, 0, 0)
+
+local ScoreText=display.newText("score:"..fruits.textscore(),halfW, screenH*0.3,nil,48)
+      ScoreText:setTextColor(255, 0, 0)
+
