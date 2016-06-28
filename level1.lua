83ebd118bdbbf088dc9bdecf42d652547431402d
masakazu yamaya <yamaya01masakazu04@gmail.com>
2016-06-28 11:16:10 +0900

    local追加

--- level1.lua
+++ level1.lua
@@ -1,1 +1,13 @@
 level1.lua
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+
+local physics = require "physics"
+physics.start(); physics.pause()
+
+
+
+
+local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
