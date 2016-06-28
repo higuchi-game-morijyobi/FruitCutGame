e387da418018291cca55479512125089ea4a4ac5
masakazu yamaya <yamaya01masakazu04@gmail.com>
2016-06-28 11:34:25 +0900

    scene:create追加01

--- level1.lua
+++ level1.lua
@@ -11,3 +11,13 @@ physics.start(); physics.pause()
 
 
 local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
+
+function scene:create( event )
+
+	local sceneGroup = self.view
+
+	
+	local background = display.newRect( 0, 0, screenW, screenH )
+	background.anchorX = 0
+	background.anchorY = 0
+	background:setFillColor( .5 )
